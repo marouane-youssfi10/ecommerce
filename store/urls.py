a781072cf665b55c0c1b34c1b22ea5a1bdf0fae7
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-02 12:40:58 +0100

    Display Products by category

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.store, name='home')
+    path('', views.store, name='home'),
+    path('<slug:category_slug>/', views.store, name='products_by_category')
 ]
