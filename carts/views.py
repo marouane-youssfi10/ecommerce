5c9ebfaecfe4d9904a97f9d3a238c50766beba21
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-03 14:49:34 +0100

    get the instance of variation

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product
+from store.models import Product, Variation
 from .models import CartItem, Cart
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
@@ -14,9 +14,16 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
      if request.method == 'POST':
-          color = request.POST['color']
-          size = request.POST['size']
-          print(color, size)
+          for item in request.POST:
+               key = item
+               value = request.POST[key]
+
+               try:
+                    variation = Variation.objects.get(variation_category__iexcat=key, variation_value__iexact=value)
+                    print(variation)
+               except:
+                    print('----- except -----')
+                    pass
      product = Product.objects.get(id=product_id)
 
      # this block for to take session_id
