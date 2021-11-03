735986372228f1506d14d46b0165be906d841579
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-03 21:03:11 +0100

    implementing model forms and editing __init__ method

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,26 @@ from .models import Account
 
 
 class RegistrationForm(forms.ModelForm):
+    # add colone  to models like this
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+        'class': 'form-control'
+    }))
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password',
+        'class': 'form-control'
+    }))
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    # this function wil be add css to fields
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter first name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter phone number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter email address'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
