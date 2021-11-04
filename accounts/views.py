f8dc9904c5625da45ce5f39f68adbaa88e93ceda
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-04 13:09:56 +0100

    Account activation & decode user pk & activation the user | expire link

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
-
+from django.http import HttpResponse
 # Verification email
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
@@ -45,8 +45,8 @@ def register(request):
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send()
 
-            messages.success(request, 'Registration successful.')
-            return redirect('register')
+            messages.success(request, 'Congratulations! Your account is activated.')
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         print('-------- else --------')
         form = RegistrationForm()
@@ -78,3 +78,20 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You are logged out.')
     return redirect('login')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulations! Your account is activated.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
+
