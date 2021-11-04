5ff9eddaf5211a00ff003bc819a2f63adf254fe5
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-04 13:16:00 +0100

    create dashboard page

--- accounts/views.py
+++ accounts/views.py
@@ -95,3 +95,6 @@ def activate(request, uidb64, token):
         messages.error(request, 'Invalid activation link')
         return redirect('register')
 
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
