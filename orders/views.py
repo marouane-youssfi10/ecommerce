b590d1923261b4c0ecd62d2d49298cecc20995fc
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-05 11:51:14 +0100

    create forms in orders app

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from carts.models import CartItem
+from .forms import OrderForm
+import datetime
+from .models import Order, Payment, OrderProduct
+import json
+from store.models import Product
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
-# Create your views here.
+
+def place_order(request):
+    current_user = request.user
+
+    # If the cart count is less than or equal to 0, then redirect back to shop
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+
+    if cart_count <= 0:
+        return redirect('store')
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
